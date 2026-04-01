451e49f574648d39428ae795ac0541d4525053ae
barbara_kush <kushaeva.va@yandex.ru>
2026-04-01 17:35:50 +0300

    add colors

---
+++ lib/core/ui/colors/colors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color gray0 = Color(0xFFFFFFFF);
+  static const Color gray100 = Color(0xFFA1A1A1);
+  static const Color gray200 = Color(0xFF9E9E9E);
+  static const Color gray300 = Color(0xFF6E6E6E);
+  static const Color gray400 = Color(0xFF232323);
+  static const Color pink500 = Color(0xFFEC499A);
+  static const Color purple500 = Color(0xFFA855F7);
+  static const Color purple600 = Color(0xFF211132);
+}
